9f2f48b5e6536c47844c30fd0749f7071142b9d7
Alexandru Eduard Gitman <alexandru.eduard.gitman@ing.be>
2016-08-19 16:04:38 +0200

    Added mock controllers and updated security for them

--- src/main/java/ro/agitman/moe/middle/security/SecurityConfiguration.java
+++ src/main/java/ro/agitman/moe/middle/security/SecurityConfiguration.java
@@ -42,9 +42,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/list").access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA')")
-                .antMatchers("/newuser/**", "/delete-user-*").access("hasRole('ADMIN')")
-                .antMatchers("/edit-user-*").access("hasRole('ADMIN') or hasRole('DBA')")
+                .antMatchers("/home").access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA')")
+//                .antMatchers("/", "/list").access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA')")
+//                .antMatchers("/newuser/**", "/delete-user-*").access("hasRole('ADMIN')")
+//                .antMatchers("/edit-user-*").access("hasRole('ADMIN') or hasRole('DBA')")
+
+                .antMatchers("/admin*").access("hasRole('ADMIN')")
+
+                .antMatchers("/stud*").access("hasRole('STUDENT')")
+
+                .antMatchers("/exam*").access("hasRole('PROFESSOR')")
+                .antMatchers("/group*").access("hasRole('PROFESSOR')")
+                .antMatchers("/instance*").access("hasRole('PROFESSOR')")
+                .antMatchers("/stats*").access("hasRole('PROFESSOR')")
+
                 .and().formLogin()
                 .loginPage("/login").loginProcessingUrl("/login")
                 .usernameParameter("ssoId")
---
+++ src/main/java/ro/agitman/moe/web/controller/prof/ProfIntanceController.java
@@ -0,0 +1,9 @@
+package ro.agitman.moe.web.controller.prof;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/instance")
+public class ProfIntanceController {
+}
